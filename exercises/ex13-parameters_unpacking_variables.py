e4f3171a3740f1121d54a41d7b47e553bcc241de
Mohanavigneshwaran Annasamy <Mohanavigneshwaran.Annasamy@adp.com>
2019-06-14 14:57:14 +0530

    Completed exercises till 14 which covers printing, arguments, inputs

---
+++ exercises/ex13-parameters_unpacking_variables.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("This script name is: ", script)
+print("Your first argument value is: ", first)
+print("Your second argument value is: ", second)
+print("Your thrid argument value is: ", third)
